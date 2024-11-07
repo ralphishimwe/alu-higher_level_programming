b40c695a4a27718625b09f11c074c49cf0b85c72
root <r.ishimwe3@alustudent.com>
2024-11-07 04:15:59 -0800

    python

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Define a square class"""
+class Square:
+    """ rep... of a square class"""
+
+    def __init__(self, size):
+        """ instantation with size for our obect
+        initialization
+        """
+        self.__size = size

58a70df87c67cb3bb69b1c630a057fc222ef9f99
root <r.ishimwe3@alustudent.com>
2024-11-07 04:23:22 -0800

    python

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,11 +9,22 @@ class Square:
         """ instantation with size for our obect
         initialization
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+        """ Getter gor the private attr size"""
+    @property
+    def size(self):
+        """property getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """ method that  returns the current square area"""

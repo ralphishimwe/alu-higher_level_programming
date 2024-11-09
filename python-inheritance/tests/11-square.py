ff274096a8e7c77210d6bf27011ba130fc0ec55a
root <r.ishimwe3@alustudent.com>
2024-11-09 01:16:50 -0800

    python

--- python-inheritance/tests/11-square.py
+++ python-inheritance/tests/11-square.py
@@ -10,15 +10,16 @@ class Square(Rectangle):
     """Inherits from Rectangle."""
 
     def __init__(self, size):
-        """set the values"""
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
+        """Initializes data."""
 
-    def area(self):
-        """Area of Square"""
-        return self.__size ** 2
+        self.integer_validator("size", size)
+        self.__size = size
 
     def __str__(self):
         """Returns [Square] <width>/<height>."""
-        return super().__str__()
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """Area of Square"""
+
+        return self.__size ** 2

60662aa6d698807c7d3e1f514da7a2e3c03e3946
root <r.ishimwe3@alustudent.com>
2024-11-16 02:32:10 -0800

    SQL

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- RECORDS: List of all shows with their genre_id
+SELECT s.`title`, g.`genre_id`
+ FROM `tv_shows` AS s
+	INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
